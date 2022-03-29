ce9aa18f5f04614c5b4b565c218df500f3b006b8
ferlor <fernandot.loera@gmail.com>
2022-03-29 11:27:18 +0200

    initial commit

---
+++ src/app/maps/services/places.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlacesService {
+
+  public userLocation?:[number,number];
+
+  get isUserLocationReady():boolean{
+
+    return !!this.userLocation;
+
+  }
+
+  constructor() {
+
+    this.getUserLocation();
+   }
+
+  
+  public async getUserLocation(): Promise<[number, number]>{
+
+    return new Promise((resolve,reject)=>{
+
+      navigator.geolocation.getCurrentPosition(
+        ({coords})=>{
+          this.userLocation=[coords.longitude,coords.latitude]
+          resolve(this.userLocation);
+        },
+        (err)=>{
+          alert('Geolocation cannot be obtained')
+          console.log(err);
+          reject();
+        }
+          
+      );
+    });
+  }
+
+}
