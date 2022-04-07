5f6e9b5fe2d5351fa67ad563ceb150a6ce9bc7d3
ferlor <fernandot.loera@gmail.com>
2022-04-07 12:17:08 +0200

    add map

--- src/app/maps/components/map-view/map-view.component.ts
+++ src/app/maps/components/map-view/map-view.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import {Map} from 'mapbox-gl';
 import { PlacesService } from '../../services/places.service';
 
 @Component({
@@ -6,12 +7,24 @@ import { PlacesService } from '../../services/places.service';
   templateUrl: './map-view.component.html',
   styleUrls: ['./map-view.component.css']
 })
-export class MapViewComponent implements OnInit {
+export class MapViewComponent implements AfterViewInit {
+
+  @ViewChild('mapDiv')
+  mapDivElement!: ElementRef
 
   constructor( private placesService: PlacesService) { }
 
-  ngOnInit(): void {
-    console.log(this.placesService.userLocation);
+  ngAfterViewInit(): void {
+
+    if(!this.placesService.userLocation) throw Error("There is not useLocation");
+    
+    const map = new Map({
+      container: this.mapDivElement.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11', // style URL
+      center: this.placesService.userLocation,
+      zoom: 14,
+      });
+    
   }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import Mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+ 
+Mapboxgl.accessToken = 'pk.eyJ1IjoiZmVybmFuZG9sb2VyYSIsImEiOiJjbDFvczhlNmwwM3Y5M2RvYWh3Yjdoa2ljIn0.XRbicWiJ2Qn6luzDyz0b1g';
+
  if(!navigator.geolocation){
    alert('Browser does not support geolocation ');
    throw new Error('Browser does not support geolocation ');
