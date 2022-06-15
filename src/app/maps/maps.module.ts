11d2d5d29e845637da2555a89dc38b614e41cb8b
Ing. Fernando Loera <fernandot.loera@gmail.com>
2022-06-15 11:15:52 +0200

    add service to control the map

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MapScreenComponent } from './screens/map-screen/map-screen.component';
 import { MapViewComponent } from './components/map-view/map-view.component';
 import { LoadingComponent } from './components/loading/loading.component';
 import { AngularLogoComponent } from './components/angular-logo/angular-logo.component';
 import { BtnMyLocationComponent } from './components/btn-my-location/btn-my-location.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { SearchResultsComponent } from './components/search-results/search-results.component';
 
 
 
@@ -14,7 +16,9 @@ import { BtnMyLocationComponent } from './components/btn-my-location/btn-my-loca
     MapViewComponent,
     LoadingComponent,
     AngularLogoComponent,
-    BtnMyLocationComponent
+    BtnMyLocationComponent,
+    SearchBarComponent,
+    SearchResultsComponent,
   ],
   imports: [
     CommonModule
